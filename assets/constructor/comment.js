d0675e197e063134d10475fff9845983fb6395f5
yingbing-developer <1014295211@qq.com>
2022-04-15 12:11:14 +0800

    代码保存

--- assets/constructor/comment.js
+++ assets/constructor/comment.js
@@ -1,13 +1,14 @@
 //评论
 
 export default class Comment {
-	constructor({id, avatar, title, subtitle, content, subComment, extra}) {
+	constructor({id, avatar, title, subtitle, content, subComment, masterComment, extra}) {
 	  this.id = id
-	  this.avatar = avatar
+	  this.avatar = avatar || ''
 	  this.title = title
 	  this.subtitle = subtitle || ''
 	  this.content = content || ''
 	  this.subComment = subComment || []
+	  this.masterComment = masterComment || []
 	  this.type = 'comment'//类型
 	  this.extra = extra || {}//附加字段
 	}
