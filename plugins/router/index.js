b932623e87611fb8510f77af4135acd1be1ca029
yingbing-developer <1014295211@qq.com>
2022-06-22 01:14:26 +0800

    代码保存

--- plugins/router/index.js
+++ plugins/router/index.js
@@ -2,14 +2,14 @@ import Vue from 'vue';
 import Router from './router.js';
 
 const guard = new Router();
-const tabbar = ['/pages/book/index', '/pages/comic/index', '/pages/music/index', '/pages/user/index']
+// const tabbar = ['/pages/book/index', '/pages/comic/index', '/pages/music/index', '/pages/user/index']
 /**
  * 路由前置守卫
 */
 guard.beforeEach((to, from, next) => {
-	if ( tabbar.indexOf(from.url) > -1 && tabbar.indexOf(to.path) == -1 ) {
-		uni.hideTabBar()
-	}
+	// if ( tabbar.indexOf(from.url) > -1 && tabbar.indexOf(to.path) == -1 ) {
+	// 	uni.hideTabBar()
+	// }
 	next();
 });
 
